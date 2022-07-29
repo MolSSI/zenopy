15f7665ea2c5e3b8faf5f48d31eb2adf86e414c9
SinaMostafanejad <m_mostafanejad@hotmail.com>
2022-07-29 16:22:45 -0400

    starts implementing the depositions module

--- zenopy/entities/depositions.py
+++ zenopy/entities/depositions.py
@@ -5,9 +5,20 @@
 """
 
 import logging
-from entity import Entity
+import requests
+import pprint
+from datetime import datetime, timezone
+
+import entities
+from entities.entity import Entity
+from entities import record
+from errors import zenodo_error
 
 logger = logging.getLogger(__name__)
+
+deposition_form = {
+
+}
 
 class Depositions(Entity):
     """Deposit provides API for uploading and editing published outputs
@@ -20,39 +31,53 @@ class Depositions(Entity):
         doi_url: str = None,
         files: list = None,
         id_: int = None,
-        metadata: object = None,
+        metadata: dict = None,
         modified: datetime = None,
         owner: int = None,
         record_url: str = None,
-        state: str = 'inprogress',
+        state: str = 'unsubmitted',
         submitted: bool = False,
         title: str = None
     ):
         Entity.__init__(self)
-        self._base_deposit_url = self._base_url + "/deposit/depositions"
-        self._response = None
+        self._deposits_url = self._base_url + "/deposit/depositions"
+        self.data = {}
 
         if created is None or created == "":
-            self._created = datetime.now(timezone.utc).isoformat()
-        self.doi = doi
-        self.doi_url = doi_url
-        self.files = files
-        self.id = id_
-        # if metadata is None:
-
+            self.data["created"] = datetime.now(timezone.utc).isoformat()
+        self.data["doi"] = doi
+        self.data["doi_url"] = doi_url
+        self.data["files"] = files
+        self.data["id"] = id_
+        if metadata is None:
+            self.data["metadata"] = {}
+            self.data["metadata"]["title"] = title
         if modified is None or modified == "":
-            self._modified = datetime.now(timezone.utc).isoformat()
-        self.owner = owner
-        self.record_url = record_url
-        if state in ["inprogress", "done", "error"]:
-            self.state = state
+            self.data["modified"] = datetime.now(timezone.utc).isoformat()
+        self.data["owner"] = owner
+        self.data["record_url"] = record_url
+        if state in ["unsubmitted", "inprogress", "done", "error"]:
+            self.data["state"] = state
         else:
             raise ValueError(
                 f"The given state ({state}) is not permitted.\n"
-                "The valid states are 'inprogress', 'done', 'error'."
+                "The valid states are 'unsubmitted', 'inprogress', 'done', or 'error'."
             )
-        self.submitted = submitted
-        self.title = title
+        self.data["submitted"] = submitted
+        self.data["title"] = title
+
+    def create_deposition(self):
+        """Create a new deposition/record object for uploading to Zenodo."""
+        # r = requests.post(url=url, json={}, params=params)
+        # r = requests.post(url=url, json={}, params=params, headers=headers)
+        # r = requests.post(url=url, data="{}", params=params, headers=headers)
+        response = requests.post(url=self._deposits_url, json={}, params=self._params)
+
+        status_code = response.status_code
+        if status_code != 201:
+            zenodo_error(response.status_code)
+
+        return record.Record(record=response)
 
     def list_depositions(
         self,
@@ -73,3 +98,7 @@ class Depositions(Entity):
             
         response = requests.get(self._base_deposit_url, params = tmp_params)
         return response.json()
+
+    def update_deposition():
+        """Update an existing deposition resource."""
+        pass
