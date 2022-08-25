a881cfac282dfb4a5f3ebd4464d860f0ed82896f
SinaMostafanejad <m_mostafanejad@hotmail.com>
2022-08-25 17:49:29 -0400

    finishes the first version of create_deposition_files fxn

--- zenopy/entities/deposition_files.py
+++ zenopy/entities/deposition_files.py
@@ -4,11 +4,13 @@
 
 """
 
+from xml.dom import ValidationErr
 import requests
-# import validators
+import validators
 # import json
 # from datetime import datetime, timezone
 from entities.record import Record
+from pathlib import Path
 # from entities.metadata import ( 
 #     upload_types, publication_types, image_types, creators_metadata,
 #     access_rights,
@@ -27,13 +29,48 @@ class _DepositionFiles(object):
         self._deposits_url = client._base_url + "/deposit/depositions/@id/files"
         self.data = {}
     
-    def create_deposition_file(self) -> Record:
+    def create_deposition_file(self, file_path: (str | Path) = None, bucket_url: str = None, record: Record = None) -> Record:
         """Upload a new file (into an existing deposition record)."""
-        response = requests.post(url=tmp_url, json={}, params=self._params)
-        status_code = response.status_code
-        if status_code not in [200, 201]:
-            zenodo_error(status_code)
-        return Record(self._client, record=response)
+        if file_path is not None and file_path != "":
+            if isinstance(file_path, str):
+                path = Path(file_path).expanduser()
+            elif isinstance(file_path, Path):
+                path = file_path.expanduser()
+            else:
+                raise TypeError(
+                    "The 'file_path' should be of str or Path type."
+                )
+            if path.exists():
+                if bucket_url is not None and isinstance(bucket_url, str):
+                    tmp_url = bucket_url.strip().rstrip("/")
+                elif record is not None and isinstance(record, Record):
+                    tmp_url = record.data["links"]["bucket"]
+                    print(tmp_url)
+                else:
+                    raise ValueError(
+                        "Both 'bucket_url' and 'record' are of None or wrong type."
+                    )
+                is_valid = validators.url(tmp_url)
+                is_valid = tmp_url.startswith(self._client._base_url + "/files")
+                if is_valid:
+                    with open(path, "rb") as fp:
+                        response = requests.put(
+                        url="%s/%s" % (tmp_url, path.name),
+                        data=fp,
+                        params=self._params,
+                        )
+                        status_code = response.status_code
+                        if status_code not in [200, 201]:
+                            zenodo_error(status_code)
+                        return Record(self._client, record=response)
+                else:
+                    RuntimeError(
+                        f"The provided path ({path}) is not valid."
+                    )
+        else:
+            ValueError(
+                "The 'file_path' argument cannot be None or empty."
+            )
 
     def list_deposition_files(self, id_: int = None) -> list[Record]:
         """List all deposition files for a given deposition"""
