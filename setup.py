63cc838a08d13ded23ee94f439fa60b3b96eff4f
SinaMostafanejad <m_mostafanejad@hotmail.com>
2022-09-11 22:28:54 -0400

    edits setup.py and adds MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,59 +1,59 @@
-"""
-ZenoPy
-A Python wrapper package for Zenodo API
-"""
 import sys
 from setuptools import setup, find_packages
 import versioneer
-
-short_description = __doc__.split("\n")
 
 # from https://github.com/pytest-dev/pytest-runner#conditional-requirement
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 
+short_description = "zenopy: A Python wrapper package for Zenodo API"
+
 try:
-    with open("README.md", "r") as handle:
-        long_description = handle.read()
-except:
-    long_description = "\n".join(short_description[2:])
+    with open("README.md", "r") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = short_description
 
+with open('requirements.txt') as fd:
+    requirements = fd.read()
 
-setup(
-    # Self-descriptive entries which should always be present
-    name='zenopy',
-    author='Paul Saxe',
-    author_email='psaxe@vt.edu',
-    description=short_description[0],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    license='BSD-3-Clause',
-
-    # Which Python importable modules should be included when your package is installed
-    # Handled automatically by setuptools. Use 'exclude' to prevent some specific
-    # subpackage(s) from being added, if needed
-    packages=find_packages(),
-
-    # Optional include package data to ship with your package
-    # Customize MANIFEST.in if the general case does not suit your needs
-    # Comment out this line to prevent the files from being packaged with your software
-    include_package_data=True,
-
-    # Allows `setup.py test` to work correctly with pytest
-    setup_requires=[] + pytest_runner,
-
-    # Additional entries you may want simply uncomment the lines you want and fill in the data
-    # url='http://www.my_package.com',  # Website
-    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
-    # platforms=['Linux',
-    #            'Mac OS-X',
-    #            'Unix',
-    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
-    # python_requires=">=3.5",          # Python version restrictions
-
-    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
-    # zip_safe=False,
-
-)
+if __name__ == "__main__":
+    setup(
+        name='zenopy',
+        description=short_description,
+        author="Mohammad Mostafanejad",
+        author_email='smostafanejad@vt.edu',
+        url='https://github.com/MolSSI/zenopy',
+        license="GNU Lesser General Public License v3",
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        packages=find_packages(include=['zenopy']),
+        setup_requires=[] + pytest_runner,
+        include_package_data=True,
+        install_requires=requirements,
+        extras_require={
+            "docs": [
+                "sphinx==1.2.3",
+                "sphinxcontrib-napoleon",
+                "sphinx_rtd_theme",
+                "numpydoc",
+            ],
+            "tests": ["pytest", "pytest-cov"],
+            "lint": ["black"],
+        },
+        test_suite='tests',
+        tests_require=["pytest", "pytest-cov"],
+        keywords='zenopy',
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            ('License :: OSI Approved :: GNU Lesser General Public License v3 or '
+             'later (LGPLv3+)'),
+            'Natural Language :: English',
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.10',
+        ],
+        zip_safe=False,
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+    )
