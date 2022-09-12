aa4359812479a847ff579cb36a380b61268ade8f
SinaMostafanejad <m_mostafanejad@hotmail.com>
2022-09-11 22:28:11 -0400

    edits __init__.py file

--- zenopy/__init__.py
+++ zenopy/__init__.py
@@ -1,15 +1,27 @@
+# -*- coding: utf-8 -*-
+
 """
-ZenoPy
+zenopy
 A Python wrapper package for Zenodo API
 """
 
-# Add imports here
-import client
-import entities
+import textwrap
+
+# Bring up the classes so that they appear to be directly in
+# the zenopy package.
+
+from zenopy.client import Zenodo    # noqa: E402
+from zenopy import metadata # noqa: E402
+
+wrap_text = textwrap.TextWrapper(width=120)
+wrap_stdout = textwrap.TextWrapper(width=120)
 
 # Handle versioneer
-from ._version import get_versions
+from ._version import get_versions  # noqa: E402
+
+__author__ = """Mohammad Mostafanejad"""
+__email__ = "smostafanejad@vt.edu"
 versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
 del get_versions, versions
